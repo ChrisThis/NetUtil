d2f88c4f1eafc0883bdfd853ebd5da7f1b68e018
ChrisThis <christopher.franz.egger@gmail.com>
2022-08-08 14:12:44 +0200

    First commit

---
+++ NetUtil/Checker/CheckPort.cs
@@ -0,0 +1,244 @@
+﻿//*************************************************************
+//*************************************************************
+// Autor: Egger Christopher 
+// Company: apic GmbH
+// Datum: 08.008.2022                                 
+// Version: 1.0                                              
+//                                                          
+// Description:                                                         
+// Check if an port is reachabel or not
+// Changelog:
+//                                                          
+//*************************************************************
+//*************************************************************
+
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Timers;
+using System.Threading;
+using System.Windows.Threading;
+using System.Net;
+
+
+namespace NetUtil.Checker
+{
+    /// <summary>
+    /// Check port event arguments
+    /// </summary>
+    public class CheckPortEventArgs : EventArgs
+    {
+        public CheckPortEventArgs(bool isFirstResult, string host, int port, bool connected, TimeSpan elapsed)
+        {
+            IsFirstResult = isFirstResult;
+            Host = host;
+            Port = port;
+            Connected = connected;
+            Elapsed = elapsed;
+        }
+
+        public bool IsFirstResult { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool Connected { get; set; }
+        public TimeSpan Elapsed { get; set; }
+    }
+
+    /// <summary>
+    /// Check port
+    /// </summary>
+    public class CheckPort
+    {
+
+        #region Getter / Setters
+
+        /// <summary>
+        /// How many retries should we to
+        /// </summary>
+        public int Retries { get; set; }
+
+        /// <summary>
+        /// Host to check
+        /// </summary>
+        public string Host { get; private set; }
+
+        /// <summary>
+        /// Port to check
+        /// </summary>
+        public int Port { get; private set; }
+
+        /// <summary>
+        /// Is port chcker running
+        /// </summary>
+        public bool IsRunning { get; private set; }
+
+        #endregion
+
+        #region pirvate members
+
+        /// <summary>
+        /// How many retries we have done
+        /// </summary>
+        private int _retries = 0;
+
+        /// <summary>
+        /// Tcp client
+        /// </summary>
+        private TcpClient _tcpClient = null;    
+
+        /// <summary>
+        /// Execution timer
+        /// </summary>
+        private System.Timers.Timer _executionTimer = null;
+
+        /// <summary>
+        /// Asyncron threadsafe event handler
+        /// </summary>
+        AsyncOperation _asyncOp = null;
+
+        #endregion
+
+        #region event handler
+
+        /// <summary>
+        /// Check result event
+        /// </summary>
+        public event EventHandler<CheckPortEventArgs> NewCheckResult;
+
+        #endregion
+
+        #region C'tor
+
+        /// <summary>
+        /// C'tor
+        /// </summary>
+        public CheckPort()
+        {
+            //Asyncon event handler
+            _asyncOp = AsyncOperationManager.CreateOperation(null);
+
+            //Set default values
+            Retries = 4;
+            IsRunning = false;
+            _retries = 0;
+        }
+
+        #endregion
+
+        #region public functions
+
+        /// <summary>
+        /// Start checking
+        /// </summary>
+        public void StartCheck(string host, int port)
+        {            
+            if (_executionTimer == null)
+            {  
+                _executionTimer = new System.Timers.Timer(500);
+                _executionTimer.Elapsed += ExecutionTimer_Elapsed;
+            }
+
+            if (IsRunning) return;
+
+            Host = host;
+            Port = port;
+            _retries = 0;
+            IsRunning = true;
+            _executionTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Start checking
+        /// </summary>
+        public void Cancle()
+        {
+            if(IsRunning)
+            {
+                _executionTimer.Enabled = false;
+                IsRunning = false;
+            } 
+        }
+
+        #endregion
+
+        #region private functions
+
+        /// <summary>
+        /// Send an check result
+        /// </summary>
+        private void CreateNewCheckResult(bool isFirstResult, string host, int port, bool connected, TimeSpan elapsed)
+        {
+            CheckPortEventArgs arg = new CheckPortEventArgs(isFirstResult, host, port, connected, elapsed);
+
+            _asyncOp.Post(new SendOrPostCallback(delegate (object obj)
+            {
+                OnCreateNewCheckResult(arg);
+            }), null); 
+        }
+
+        /// <summary>
+        /// Send an check result event handler
+        /// </summary>
+        protected void OnCreateNewCheckResult(CheckPortEventArgs e)
+        {
+            EventHandler<CheckPortEventArgs> tmpHandler = NewCheckResult;
+            tmpHandler?.Invoke(this, e);
+        }
+
+        #endregion
+
+        #region events
+
+        /// <summary>
+        /// Execution timer tick event
+        /// </summary>
+        private void ExecutionTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _executionTimer.Enabled = false;
+            if (!IsRunning) return;
+
+            IPAddress ipAddress = IPAddress.Parse(Host);
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, Port);
+
+            if (_tcpClient == null) _tcpClient = new TcpClient();
+
+            bool connected = false;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            try
+            {
+                //  _tcpClient.Connect(endPoint);
+                _tcpClient.ConnectAsync(Host, Port).Wait(2000);
+                if (_tcpClient.Connected)
+                {
+                    _tcpClient.Close();
+                    connected = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            _tcpClient = null;
+            stopWatch.Stop();
+            if (!IsRunning) return;
+            CreateNewCheckResult(_retries == 0,Host, Port, connected, stopWatch.Elapsed);
+
+            _retries += 1;
+            if (_retries >= Retries)
+            {
+                IsRunning = false;
+                _executionTimer.Enabled = false;
+            }
+            else
+            {
+                _executionTimer.Enabled = true;
+           }
+        }
+
+        #endregion
+    }
+}
---
+++ NetUtil/MainWindow.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using NetUtil.Checker;
+using System.Text.RegularExpressions;
+
+
+namespace NetUtil
+{
+    /// <summary>
+    /// Logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region pirvate members
+
+        /// <summary>
+        /// Port checker
+        /// </summary>
+        CheckPort _checkPort = null;
+
+        #endregion
+
+        #region C'tor
+
+        /// <summary>
+        /// C'tor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region HMI events
+
+        /// <summary>
+        /// Combo box port TIA selection
+        /// </summary>
+        private void cmbBx_portTiaSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = this.cmbBxPortTiaSelection.SelectedItem as ComboBoxItem;
+            if (selectedItem == null) return;
+            var si = selectedItem.Content;
+            if (si == null) return;
+            this.txtBx_CheckPort.Text = GetPortFromComboBox(si.ToString());
+        }
+
+        /// <summary>
+        /// Combo box port Zenon selection
+        /// </summary>
+        private void cmbBx_portZenonSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = this.cmbBxPortZenonSelection.SelectedItem as ComboBoxItem;
+            if (selectedItem == null) return;
+            var si = selectedItem.Content;
+            if (si == null) return;
+            this.txtBx_CheckPort.Text = GetPortFromComboBox(si.ToString());
+        }
+
+        /// <summary>
+        /// Combo box port Common selection
+        /// </summary>
+        private void cmbBx_portCommonSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = this.cmbBxPortCommonSelection.SelectedItem as ComboBoxItem;
+            if (selectedItem == null) return;
+            var si = selectedItem.Content;
+            if (si == null) return;
+            this.txtBx_CheckPort.Text = GetPortFromComboBox(si.ToString());
+        }
+
+        /// <summary>
+        /// Button port checker start
+        /// </summary>
+        private void btnCheckPortPing_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string ip = this.txtBx_CheckPortIP.Text;
+                string port = this.txtBx_CheckPort.Text;
+                this.txtBlock_CheckPortResult.Text = "Checking ...";
+
+                if (_checkPort == null)
+                {
+                    _checkPort = new CheckPort();
+                    _checkPort.NewCheckResult += HandlePortCheckerResultEvent;
+                }
+
+                if (!_checkPort.IsRunning) _checkPort.StartCheck(ip, Int32.Parse(port));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }   
+        }
+
+        /// <summary>
+        /// Button cancle
+        /// </summary>
+        private void btnCancle_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_checkPort != null)
+                {
+                    if(_checkPort.IsRunning)
+                    {
+                        _checkPort.Cancle();
+                        this.txtBlock_CheckPortResult.Inlines.Add("Cancled");
+                    }                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        /// <summary>
+        /// Textbox ort previe text input event
+        /// </summary>
+        // private void txtBx_CheckPortPrevTextInput(object sender, DataObjectPastingEventArgs e)
+        private void txtBx_CheckPortPrevTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Port checker result event
+        /// </summary>
+        string GetPortFromComboBox(string cmb)
+        {
+            try
+            {
+                if (cmb == null) return "0";
+
+                var list = cmb.ToString().Split(':');
+                if (list.Length <= 1) return "0";
+
+                string s = list.ElementAt(1).Trim();
+                return list.ElementAt(1).Trim();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return "0";
+            }
+        }
+        #endregion
+
+        #region Result events
+
+        /// <summary>
+        /// Port checker result event
+        /// </summary>
+        void HandlePortCheckerResultEvent(object sender, CheckPortEventArgs e)
+        {
+            Console.WriteLine(e.Host, e.Elapsed);
+            if (e.IsFirstResult) this.txtBlock_CheckPortResult.Text = "";
+
+            Run runHost = new Run(e.Host);
+            runHost.FontWeight = FontWeights.Bold;
+            this.txtBlock_CheckPortResult.Inlines.Add(runHost);
+
+            this.txtBlock_CheckPortResult.Inlines.Add(" port:");
+
+            Run runPort = new Run(e.Port.ToString());
+            runPort.FontWeight = FontWeights.Bold;
+            this.txtBlock_CheckPortResult.Inlines.Add(runPort);
+
+            this.txtBlock_CheckPortResult.Inlines.Add(" ");
+
+            if (e.Connected)
+            {
+                Run run = new Run("OPEN");
+                run.Foreground = Brushes.Green;
+                run.FontWeight = FontWeights.Bold;
+                this.txtBlock_CheckPortResult.Inlines.Add(run);
+                this.txtBlock_CheckPortResult.Inlines.Add(" ");
+            }
+            else
+            {
+                Run run = new Run("CLOSED");
+                run.Foreground = Brushes.Red;
+                run.FontWeight = FontWeights.Bold;
+                this.txtBlock_CheckPortResult.Inlines.Add(run);
+                this.txtBlock_CheckPortResult.Inlines.Add(" ");
+            }
+
+            int ms = 0;
+            try
+            {
+                ms = Convert.ToInt32(e.Elapsed.TotalMilliseconds);
+            }
+            catch (Exception)
+            {
+            }
+
+            this.txtBlock_CheckPortResult.Inlines.Add(ms.ToString());
+            this.txtBlock_CheckPortResult.Inlines.Add("ms");
+            this.txtBlock_CheckPortResult.Inlines.Add("\r\n");
+        }
+
+        #endregion
+    }
+}
